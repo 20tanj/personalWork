3366f530d0ce0ac5d21c4c23558423ea2a9ee8f4
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-13 13:57:34 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,18 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JJJJJJJ JJJJJJJ TTTTTTT");
+		System.out.println("   J       J       T");
+		System.out.println("   J       J       T");
+		System.out.println("J  J    J  J       T");
+		System.out.println(" JJ      JJ        T");
+		
+		
+		
+		
+	}
+
+}
