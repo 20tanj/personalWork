19996835b8484a3254e496f19cbf3c570ae79695
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-13 13:42:32 -0500

    Student Schedule

---
+++ src/outputExamples/StudentSchedule.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class StudentSchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jacob Tan");
+		System.out.println("AP BC Calc");
+		System.out.println("Spanish");
+		System.out.println("PLTW Human Body Systems");
+		System.out.println("AP Computer Science A");
+	}
+
+}
