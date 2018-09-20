4f44f4f847f9ff7a8f392fdcc11730f0d4589bce
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-20 13:40:31 -0500

    FortuneTeller

---
+++ src/inputExamples/FortuneTeller.java
@@ -0,0 +1,22 @@
+package inputExamples;
+import java.util.Scanner;
+public class FortuneTeller {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = -999;
+Scanner input = new Scanner(System.in);
+System.out.println("How many years old are you?");
+age= input.nextInt();
+int weight = -999;
+System.out.println("How many pounds do you weight?");
+weight =input.nextInt();
+int luckynumber= -999;
+System.out.println("What is your lucky number?");
+luckynumber = input.nextInt();
+System.out.println("I see...");
+System.out.println("43 years from now...");
+System.out.println("You will be reside number 17180");
+System.out.println("at the old folks home");
+	}
+
+}
