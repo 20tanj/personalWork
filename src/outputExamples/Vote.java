87d9c03ad151ba4df3a23130af2ccee351af2934
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-20 13:10:08 -0500

    Voting

---
+++ src/outputExamples/Vote.java
@@ -0,0 +1,18 @@
+package outputExamples;
+import java.util.Scanner;
+public class Vote {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean answer = true;
+Scanner input = new Scanner(System.in);
+System.out.println("Did you vote?");
+answer= input.nextBoolean();
+System.out.println(answer + "you voted");
+
+
+
+
+
+	}
+
+}
