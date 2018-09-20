14278c3c61f130bbbe1fd69ba4e5315a6190b4be
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-20 13:09:04 -0500

    Finished Monkey Height

--- src/outputExamples/MonkeyHeight.java
+++ src/outputExamples/MonkeyHeight.java
@@ -1,9 +1,16 @@
 package outputExamples;
-
+import java.util.Scanner;
 public class MonkeyHeight {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+double height = -999;
+Scanner input = new Scanner(System.in);
+System.out.println("How tall is a monkey?");
+height=input.nextDouble();
+System.out.println("The height of a monkey is" + height + "inches");
+
+
+
 
 	}
 
