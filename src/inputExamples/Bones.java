886f52176a59e93b381f4af2b795aa29fe534b93
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-20 13:16:23 -0500

    Bones

---
+++ src/inputExamples/Bones.java
@@ -0,0 +1,18 @@
+package inputExamples;
+import java.util.Scanner;
+public class Bones {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int brokenbones = -999;
+Scanner input = new Scanner(System.in);
+System.out.println("How many bones have you broken?");
+brokenbones= input.nextInt();
+System.out.println("You have broken"+ brokenbones + "bones");
+
+
+
+
+
+	}
+
+}
