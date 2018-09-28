0650a88d0da52bdf15c30565719ed49afcc35556
Jacob Tan <20tanj@elmbrookstudents.org>
2018-09-28 14:56:25 -0500

    CarFriendGreeter

---
+++ src/objectOrientated/Car.java
@@ -0,0 +1,15 @@
+package objectOrientated;
+//import statements at the top
+
+public class Car {
+
+	//instance fields - data storage
+	
+	// constructors- methods for constructing the object
+	
+	//default constructor
+	public Car() {
+	//Statements to initialize the state of the object
+	}
+//methods- things the object can do
+}
---
+++ src/objectOrientated/Greeter.java
@@ -0,0 +1,17 @@
+package objectOrientated;
+//import statements at the top
+
+public class Greeter {
+
+	//instance fields - data storage
+	
+	// constructors- methods for constructing the object
+	
+	//default constructor
+	
+	public Greeter() {
+	//Statements to initialize the state of the object
+
+	}
+//methods- things the object can do
+}
