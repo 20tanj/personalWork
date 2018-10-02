489a6d12f4a2bfed71b72267185ff85eb5c42505
Jacob Tan <20tanj@elmbrookstudents.org>
2018-10-02 13:50:57 -0500

    GreeterFriend

---
+++ src/objectOrientated/GreeterFriend.java
@@ -0,0 +1,16 @@
+package objectOrientated;
+
+public class GreeterFriend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Greeter bob = new Greeter();
+Friend john = new Friend();
+bob.sayHello();
+bob.sayHello();
+bob.sayHello();
+john.sayMessage();
+bob.sayHello();
+	}
+
+}
