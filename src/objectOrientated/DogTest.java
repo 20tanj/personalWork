27624f1df9296a41ff344206749a441d9ba07a84
Jacob Tan <20tanj@elmbrookstudents.org>
2018-10-02 13:50:48 -0500

    DogTest

---
+++ src/objectOrientated/DogTest.java
@@ -0,0 +1,12 @@
+package objectOrientated;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Dog griffin = new Dog();
+griffin.attack();
+griffin.woof();
+	}
+
+}
