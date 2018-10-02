26a8e90b40dd5427c1b57a31d7350022885f7f5d
Jacob Tan <20tanj@elmbrookstudents.org>
2018-10-02 14:55:08 -0500

    KidsClass

---
+++ src/objectOrientated/KidClass.java
@@ -0,0 +1,27 @@
+package objectOrientated;
+//import statements at the top
+
+public class KidClass {
+
+	//instance fields - data storage
+	
+	// constructors- methods for constructing the object
+	
+	//default constructor
+	public KidClass() {
+	//Statements to initialize the state of the object
+	}
+//methods- things the object can do
+public void sayMama()
+{
+	System.out.println("Mama");
+}
+public void sayFood()
+{
+	System.out.println("I want food");
+}
+public void sayStink()
+{
+	System.out.println("I stink");
+}
+}
