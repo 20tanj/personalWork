916310ac1fe297056cbc298de5bcbaeebe71be9c
Jacob Tan <20tanj@elmbrookstudents.org>
2018-10-02 14:54:59 -0500

    CarClass

---
+++ src/objectOrientated/CarClass.java
@@ -0,0 +1,27 @@
+package objectOrientated;
+//import statements at the top
+
+public class CarClass {
+
+	//instance fields - data storage
+	
+	// constructors- methods for constructing the object
+	
+	//default constructor
+	public CarClass() {
+	//Statements to initialize the state of the object
+	}
+//methods- things the object can do
+public void sayOdometer()
+{
+	System.out.println("There are  amount of miles left");
+}
+public void sayFlat()
+{
+	System.out.println("You have a flat tire");
+}
+public void sayThirsty()
+{
+	System.out.println("You are out of gas");
+}
+}
