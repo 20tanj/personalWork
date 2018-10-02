100e87b80405a20968331941121082d8c54c6c26
Jacob Tan <20tanj@elmbrookstudents.org>
2018-10-02 14:55:15 -0500

    NeighborClass

---
+++ src/objectOrientated/NeighborClass.java
@@ -0,0 +1,27 @@
+package objectOrientated;
+//import statements at the top
+
+public class NeighborClass {
+
+	//instance fields - data storage
+	
+	// constructors- methods for constructing the object
+	
+	//default constructor
+	public NeighborClass() {
+	//Statements to initialize the state of the object
+	}
+//methods- things the object can do
+public void sayJoke()
+{
+	System.out.println("You");
+}
+public void sayMoney()
+{
+	System.out.println("I need some money");
+}
+public void sayConversation()
+{
+	System.out.println("You suck");
+}
+}
