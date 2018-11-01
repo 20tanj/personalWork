2cacb1f96949d0fd1676901756cbe58f9c768a23
Jacob Tan <20tanj@elmbrookstudents.org>
2018-11-01 14:09:25 -0500

    NewCircle

---
+++ src/decisions/NewCircle.java
@@ -0,0 +1,20 @@
+package decisions;
+
+public class NewCircle {
+private double radius;
+public NewCircle(){
+	radius=10;
+}
+public NewCircle(double newR){
+	radius=newR;
+}
+
+public double getCircumfrance(){
+	return radius*2*Math.PI;
+}
+
+
+
+
+
+}
