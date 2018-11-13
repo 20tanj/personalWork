6db5c6f92d6c59f643711ed738151d5fc29345e8
Jacob Tan <20tanj@elmbrookstudents.org>
2018-11-13 15:04:49 -0600

    Activities for 11/13/18

--- src/strings/StringActivities.java
+++ src/strings/StringActivities.java
@@ -18,16 +18,50 @@ System.out.println(sentence);
 */
 
 //Activity 2
-
+/*
 System.out.println("Give a sentence");
 String sentence= input.nextLine();
 System.out.println(sentence.contains("bob"));
+*/
 
+//Activity 4
+/*
+System.out.println("Give a sentence");
+String sentence= input.nextLine();
+System.out.println(sentence.indexOf("X"));
+*/
 
+//Activity 5
+/*
+System.out.println("Give a sentence");
+String sentence= input.nextLine();
+System.out.println(sentence.length());
+*/
 
+//Activity 6
+/*
+System.out.println("Give a sentence");
+String sentence= input.nextLine();
 
+	for(int character=0; character<=sentence.length(); character++)
+	{
+		System.out.println(sentence.charAt(character));
+	}
+*/
+/*
+String text="My name is Buzz Lightyear!";
+for(int i=1; i<text.length();i++)
+{
+	System.out.println(text.substring(0,i));
+}
+*/
 
+//Activity 7
 
+System.out.println("Give a sentence");
+String sentence= input.nextLine();
+System.out.println(sentence.toLowerCase());
+System.out.println(sentence);
 
 
 
