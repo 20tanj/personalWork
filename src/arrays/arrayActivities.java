cceac40ef4922b68e4388545d39874eb6694e165
Jacob Tan <20tanj@elmbrookstudents.org>
2018-12-13 14:10:10 -0600

    array Activities 12/13/2018

--- src/arrays/arrayActivities.java
+++ src/arrays/arrayActivities.java
@@ -1,11 +1,11 @@
 package arrays;
 
 import java.util.Scanner;
-
+import objectOrientated.Greeter;
 import objectOrientated.Circle;
-
 import java.util.Random;
 import java.util.ArrayList;
+
 public class arrayActivities {
 
 	public static void main(String[] args) {
@@ -147,164 +147,106 @@ public class arrayActivities {
 		 * } System.out.println(); }
 		 */
 		/*
-		int row = 18;
-		int column = 5;
-		int min = 0;
-		int max = 100;
-
-		Friend[][] numbers = new Friend[row][column];
-		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 0; j < numbers[0].length; j++) {
-				numbers[i][j] = new Friend();
-
+		 * int row = 18; int column = 5; int min = 0; int max = 100;
+		 * 
+		 * Friend[][] numbers = new Friend[row][column]; for (int i = 0; i <
+		 * numbers.length; i++) { for (int j = 0; j < numbers[0].length; j++) {
+		 * numbers[i][j] = new Friend();
+		 * 
+		 * } } for (int i = 0; i < numbers.length; i++) { for (int j = 0; j <
+		 * numbers[0].length; j++) { int side = min + generator.nextInt(max -
+		 * min + 1); numbers[i][j].setAge(side);
+		 * 
+		 * } } for (int i = 0; i < numbers.length; i++) { for (int j = 0; j <
+		 * numbers[0].length; j++) { System.out.println(numbers[i][j].getAge());
+		 * 
+		 * } }
+		 */
+		/*
+		 * 
+		 * ArrayList<String> names= new ArrayList<String>(); names.add("Bob");
+		 * names.add("Jane"); names.add(0, "Billy");
+		 * System.out.println("Give your name"); String name=input.nextLine();
+		 * names.add(name); for(int i=0; i<names.size(); i++) {
+		 * System.out.println(names.get(i)); }
+		 */
+		/*
+		 * ArrayList<Friend> friends= new ArrayList<Friend>(); friends.add(0,
+		 * new Friend("John Smith", 45)); friends.add(0, new Friend("Jane",
+		 * 45)); friends.add(0, new Friend("Jacob", 45)); for(int i=0;
+		 * i<friends.size(); i++) { System.out.println(friends.get(i).getAge());
+		 * }
+		 */
+		/*
+		 * ArrayList<Rectangle> rectangles= new ArrayList<Rectangle>(); int
+		 * min=50; int max=70; int min2=0; int max2=399; for(int i=0; i<400;
+		 * i++) { int length=min+ generator.nextInt(max-min+1); int width=min+
+		 * generator.nextInt(max-min+1); rectangles.add(0, new Rectangle(length,
+		 * width)); } int rec=min2+ generator.nextInt(max2-min2+1); Rectangle
+		 * save= rectangles.remove(rec); System.out.println(save.getLength());
+		 * for(int k=398; k>=0; k--) {
+		 * System.out.println(rectangles.get(k).area()); }
+		 */
+		/*
+		 * ArrayList<String> names= new ArrayList<String>(); names.add("Jack");
+		 * names.add("Joey"); names.add("Jacob"); names.add("Jake");
+		 * names.add("Joe"); String first= names.remove(0); String last =
+		 * names.remove(names.size()-1); System.out.println(names);
+		 */
+		/*
+		 * ArrayList<Rectangle> rectangles= new ArrayList<Rectangle>();
+		 * rectangles.add(new Rectangle()); rectangles.add(new Rectangle());
+		 * rectangles.add(new Rectangle());
+		 * 
+		 * Rectangle last= rectangles.remove(2); rectangles.set(0, last);
+		 */
+		/*
+		 * int number= input.nextInt(); int sum=0; int[] students=new
+		 * int[number]; for(int i=0; i<students.length; i++) { students[i]=
+		 * input.nextInt(); } for(int num:students) { sum= sum+num; } int
+		 * average= (sum/number); System.out.println(average);
+		 */
+		/*
+		 * ArrayList<Double> employees= new ArrayList<Double>(); double
+		 * highest=0.0; for(int i=0; i<5;i++) { System.out.println("wage");
+		 * double wage= input.nextDouble(); employees.add(wage); }
+		 * 
+		 * for(double num:employees) { if(num>highest) { highest=num; } }
+		 * System.out.println(highest);
+		 */
+		/*
+		 * Circle[] circles = new Circle[10]; for(int i=0; i<circles.length;
+		 * i++) { circles[i]=new Circle(); System.out.println("radius"); double
+		 * radi= input.nextDouble(); circles[i].setRadius(radi); }
+		 * 
+		 * for(Circle num:circles) { System.out.println(num.area()); }
+		 */
+		/*
+		 * ArrayList<Friend> friends= new ArrayList<Friend>(); for(int i=0; i<3;
+		 * i++) { System.out.println("Give Age"); int age= input.nextInt();
+		 * input.nextLine(); System.out.println("Give Name"); String name=
+		 * input.nextLine();
+		 * 
+		 * friends.add(new Friend(name, age)); } for(int i=2; i>=0; i--) {
+		 * String namee =friends.get(i).getName(); int space=
+		 * namee.indexOf(" "); String last= namee.substring(space+1); String
+		 * first= namee.substring(0, space); System.out.println(last+ ","
+		 * +first); }
+		 */
+		/*
+		ArrayList<Greeter> bob = new ArrayList<Greeter>();
+		for (int i = 0; i < 3; i++) {
+			bob.add(new Greeter(18));
+		}
+		for (Greeter age : bob) {
+			System.out.println(age.getAge());
 			}
-		}
-		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 0; j < numbers[0].length; j++) {
-				int side = min + generator.nextInt(max - min + 1);
-				numbers[i][j].setAge(side);
-
-			}
-		}
-		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 0; j < numbers[0].length; j++) {
-				System.out.println(numbers[i][j].getAge());
-
-			}
-		}
-		*/
-		/*
+			*/
 		
-		ArrayList<String> names= new ArrayList<String>();
-		names.add("Bob");
-		names.add("Jane");
-		names.add(0, "Billy");
-	System.out.println("Give your name");
-		String name=input.nextLine();
-		names.add(name);
-		for(int i=0; i<names.size(); i++)
-		{
-			System.out.println(names.get(i));
-		}
-		*/
-		/*
-		ArrayList<Friend> friends= new ArrayList<Friend>();
-		friends.add(0, new Friend("John Smith", 45));
-		friends.add(0, new Friend("Jane", 45));
-		friends.add(0, new Friend("Jacob", 45));
-		for(int i=0; i<friends.size(); i++)
-		{
-			System.out.println(friends.get(i).getAge());
-		}
-		*/
-		/*
-		ArrayList<Rectangle> rectangles= new ArrayList<Rectangle>();
-		int min=50;
-		int max=70;
-		int min2=0;
-		int max2=399;
-		for(int i=0; i<400; i++)
-		{
-			int length=min+ generator.nextInt(max-min+1);
-			int width=min+ generator.nextInt(max-min+1);
-			rectangles.add(0, new Rectangle(length, width));
-		}
-		int rec=min2+ generator.nextInt(max2-min2+1);
-		Rectangle save= rectangles.remove(rec);
-		System.out.println(save.getLength());
-		for(int k=398; k>=0; k--)
-		{
-			System.out.println(rectangles.get(k).area());
-		}
-	*/
-		/*
-	ArrayList<String> names= new ArrayList<String>();
-	names.add("Jack");
-	names.add("Joey");
-	names.add("Jacob");
-	names.add("Jake");
-	names.add("Joe");
-	String first= names.remove(0);
-	String last = names.remove(names.size()-1);
-	System.out.println(names);
-	*/
-	/*
-		ArrayList<Rectangle> rectangles= new ArrayList<Rectangle>();
-		rectangles.add(new Rectangle());
-		rectangles.add(new Rectangle());
-		rectangles.add(new Rectangle());
-		
-		Rectangle last= rectangles.remove(2);
-		rectangles.set(0, last);
-		*/
-		/*
-		int number= input.nextInt();
-		int sum=0;
-		int[] students=new int[number];
-		for(int i=0; i<students.length; i++)
-		{
-			students[i]= input.nextInt();
-		}
-		for(int num:students)
-		{
-			sum= sum+num;
-		}
-		int average= (sum/number);
-		System.out.println(average);
-		*/
-		/*
-		ArrayList<Double> employees= new ArrayList<Double>();
-		double highest=0.0;
-		for(int i=0; i<5;i++)
-		{
-			System.out.println("wage");
-			double wage= input.nextDouble();
-			employees.add(wage);
-		}
-		
-		for(double num:employees)
-		{
-			if(num>highest)
-			{
-				highest=num;
-			}
-		}
-		System.out.println(highest);
-		*/
-		/*
-		Circle[] circles = new Circle[10];
-		for(int i=0; i<circles.length; i++)
-		{
-			circles[i]=new Circle();
-			System.out.println("radius");
-			double radi= input.nextDouble();
-			circles[i].setRadius(radi);
-		}
-		
-		for(Circle num:circles)
-		{
-			System.out.println(num.area());
-		}
-		*/
-		
-		ArrayList<Friend> friends= new ArrayList<Friend>();
-		for(int i=0; i<3; i++)
-		{
-			System.out.println("Give Age");
-			int age= input.nextInt();
-			input.nextLine();
-			System.out.println("Give Name");
-			String name= input.nextLine();
-			
-			friends.add(new Friend(name, age));
-		}
-		for(int i=2; i>=0; i--)
-		{
-			String namee =friends.get(i).getName();
-			int space= namee.indexOf(" ");
-			String last= namee.substring(space+1);
-			String first= namee.substring(0, space);
-			System.out.println(last+ "," +first);
-		}
+		pileOjunk bob=new pileOjunk();
+		System.out.println(bob.getSum(1, 2));
+		System.out.println(bob.getRandom());
+bob.setNum(62);
+System.out.println(bob.getRandom());
 	}
 }
