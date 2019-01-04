78d8982f1c216193fcfb02d5816e9d78325ca09a
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-04 15:03:10 -0600

    Interface Activities 1/4/2019

---
+++ src/interfaces/InterfaceActivities.java
@@ -0,0 +1,35 @@
+package interfaces;
+
+import java.util.Scanner;
+
+public class InterfaceActivities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+/*
+		System.out.println("length");
+		int length = input.nextInt();
+		System.out.println("width");
+		int width = input.nextInt();
+		Shape bob = new Rectangle(length, width);
+		System.out.println(bob.perimeter());
+		System.out.println(bob.area());
+		System.out.println("radius");
+		int radius = input.nextInt();
+		Shape circlebob = new Circle(radius);
+		System.out.println(circlebob.perimeter());
+		System.out.println(circlebob.area());
+*/
+/*
+		System.out.println("length");
+		int length = input.nextInt();
+		System.out.println("width");
+		int width = input.nextInt();
+		Rectangle notbob= new Rectangle(length,width);
+		Shape bob = notbob;
+*/		
+	}
+
+}
