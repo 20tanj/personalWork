751d173574086e51716e35bd01526c782ab7c028
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-10 14:08:48 -0600

    InheritanceActivities 1/10/2019

---
+++ src/Inheritance/InheritanceActivities.java
@@ -0,0 +1,12 @@
+package Inheritance;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+public class InheritanceActivities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
