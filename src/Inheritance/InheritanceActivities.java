23e909107903e32787a1e37c9f1e3d2ea3875d07
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-11 15:06:30 -0600

    Inheritance Activities 1/11/2019

--- src/Inheritance/InheritanceActivities.java
+++ src/Inheritance/InheritanceActivities.java
@@ -6,7 +6,18 @@ public class InheritanceActivities {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		Rectangle bob=new Rectangle(10, 20);
+		System.out.println(bob.perimeter());
+		System.out.println(bob.area());
+		*/
+		
+		ArrayList<Geo> meal = new <Geo> ArrayList();
+		meal.add(new Rectangle());
+		meal.add(new Trapezoid());
+		
+		
+		
 	}
 
 }
