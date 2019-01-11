72ff8e317e98df48833a9f30ff5f0fbc6922e52f
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-11 15:06:06 -0600

    Rectangle

--- src/Inheritance/Rectangle.java
+++ src/Inheritance/Rectangle.java
@@ -1,29 +1,15 @@
 package Inheritance;
 
-public class Rectangle extends Quad {
+public class Rectangle extends Parallelogram implements Geo {
 
 public Rectangle()
 {
-	Quad bob=new Quad();
-	bob.setSide1(10);
-	bob.setSide2(10);
-	bob.setSide3(20);
-	bob.setSide4(20);
+	super();
 }
 public Rectangle(double xs1, double xs2)
 {
-
-	Quad bob=new Quad();
-	bob.setSide1(xs1);
-	bob.setSide2(xs1);
-	bob.setSide3(xs2);
-	bob.setSide4(xs2);
-	}
-
-public double area()
-{
-	Quad bob=new Quad();
-	return bob.getSide1()*bob.getSide3();
+super(xs1, xs2, xs1);
 }
-	
+
+
 }
