38999a9ddd169824eb8cb9045188079df8b69a77
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-14 15:01:04 -0600

    Rectangle

--- src/Inheritance/Rectangle.java
+++ src/Inheritance/Rectangle.java
@@ -10,6 +10,16 @@ public Rectangle(double xs1, double xs2)
 {
 super(xs1, xs2, xs1);
 }
-
-
+public String toString()
+{
+	return"Rectangle:" +super.toString();
+}
+public boolean equals(Object otherObject)
+{
+	return (this.toString().equals(otherObject.toString()));
+}
+public Object clone()
+{
+	return new Rectangle(super.getSide1(),super.getSide2());
+}
 }
