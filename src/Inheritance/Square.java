aed338dc8a2b7ed01b0403117e8ec12976ad536f
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-14 15:01:11 -0600

    Square

--- src/Inheritance/Square.java
+++ src/Inheritance/Square.java
@@ -10,4 +10,16 @@ public Square(double xs1)
 {
 	super(xs1,xs1);
 }
+public String toString()
+{
+	return "Square:" +super.toString();
+}
+public boolean equals(Object otherObject)
+{
+	return (this.toString().equals(otherObject.toString()));
+}
+public Object clone()
+{
+	return new Square(super.getSide1());
+}
 }
