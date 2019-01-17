1cb6ba4069cfd9cca2fc0185dc543e75decd58fe
Jacob Tan <20tanj@elmbrookstudents.org>
2019-01-17 14:07:39 -0600

    CatTestClass

---
+++ src/Inheritance/CatTestClass.java
@@ -0,0 +1,16 @@
+package Inheritance;
+import java.util.ArrayList;
+public class CatTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<Cat> cats=new ArrayList<Cat>();
+
+		
+		
+		
+		
+		
+	}
+
+}
